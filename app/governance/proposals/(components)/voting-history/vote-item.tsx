8aedb9f7af094bbeee70380b2db3325c12a73542
cometsheepboy <cometsheepboy@gmail.com>
2025-10-21 04:06:42 -0600

    update governance page

--- app/governance/proposals/(components)/voting-history/vote-item.tsx
+++ app/governance/proposals/(components)/voting-history/vote-item.tsx
@@ -1,6 +1,5 @@
-import { Badge } from "@/components/badge"
 import { Icon } from "@/components/icon"
-import { STATUS_CONFIG, VOTE_CONFIG } from "@/config/vote"
+import { VOTE_CONFIG } from "@/config/vote"
 import { type VotedProposal } from "@/context/VotingHistoryContext"
 import { formatDistanceToNow } from "date-fns"
 import clsx from "clsx"
@@ -9,7 +8,6 @@ import { useRouter } from "next/navigation"
 
 export const VoteItem = ({ item }: { item: VotedProposal }) => {
     const router = useRouter()
-    const { color, icon } = STATUS_CONFIG[item.status]
     const { color: voteColor, icon: voteIcon } = VOTE_CONFIG[item.vote]
 
     const handleClick = () => {
@@ -19,9 +17,6 @@ export const VoteItem = ({ item }: { item: VotedProposal }) => {
     return (
         <div className={clsx(s.item, s[item.status])} onClick={handleClick}>
             <div className={s.top}>
-                <Badge status={color} icon={icon}>
-                    {item.status}
-                </Badge>
                 <div className={s.proposalId}>#{item.proposalId}</div>
                 <time dateTime={new Date(item.timestamp).toISOString()} className={s.date}>
                     <Icon icon="hugeicons:clock-04" />
