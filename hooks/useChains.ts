c6f0718827c568e14727a65f7e81e568368a0d0a
jguyet <jguyet.weoohh@gmail.com>
2025-10-24 02:18:24 +0200

    filter chains

--- hooks/useChains.ts
+++ hooks/useChains.ts
@@ -7,9 +7,13 @@ export const useChains = () => {
     queryFn: getHyperionChains
   })
 
+  const disabledChains = [56]
+
+  const filteredChains = (qHyperionChains.data || []).filter((chain) => !disabledChains.includes(chain.hyperionId))
+
   return {
-    chains: qHyperionChains.data || [],
-    heliosChainIndex: qHyperionChains.data?.findIndex(
+    chains: filteredChains,
+    heliosChainIndex: filteredChains.findIndex(
       (chain) => chain.hyperionId === 0
     ),
     isLoading: qHyperionChains.isLoading
