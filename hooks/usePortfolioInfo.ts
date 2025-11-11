a13f47f2f7eaeed3b05b1833da955248ebe35a37
jguyet <jguyet.weoohh@gmail.com>
2025-11-11 07:59:20 +0100

    clean compute

--- hooks/usePortfolioInfo.ts
+++ hooks/usePortfolioInfo.ts
@@ -28,7 +28,7 @@ export const usePortfolioInfo = (options?: UsePortfolioInfoOptions) => {
 
   const qTokenBalances = useQuery({
     queryKey: ["tokensBalance", address],
-    queryFn: () => getTokensBalance(address!, toHex(1), toHex(500)),
+    queryFn: () => getTokensBalance(address!, toHex(1), toHex(200)),
     enabled: !!address,
     staleTime: TOKEN_BALANCE_STALE_TIME,
     refetchInterval: TOKEN_BALANCE_REFETCH_INTERVAL
